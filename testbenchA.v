911915abd2e0f4dab861833c0b427f50d4655213
DimitrisTsalapatas <dhmhtrhs393@gmail.com>
2022-12-09 03:46:52 +0200

    Revert "Revert "update testbenchA.v""
    This reverts commit 3ad69268fca95f028d19555aa90823986e976b51.

--- testbenchA.v
+++ testbenchA.v
@@ -5,6 +5,8 @@ reg clk, reset, we, regce, en, di;
 wire red, green, blue;
 reg [13:0] addr;
 
+reg [7*8:0] color;      // max 7 characters string
+
 VRAM VRAM_inst(.addr(addr), .clk(clk), .reset(reset), .we(we), .regce(regce), .en(en), .di(di), .red(red), .green(green), .blue(blue));
 
 initial 
@@ -26,7 +28,26 @@ end
 always @(posedge clk)
 begin
     #100 addr <= addr + 1;
-    $monitor("addr: %d r = %d g = %d b =%d\n", addr, red, green, blue);
+    if( (red == 1) && (green == 1) && (blue == 1) )
+        color = "white";
+    else if ( (red == 1) && (green == 0) && (blue == 0) )
+        color = "red";
+    else if ( (red == 0) && (green == 1) && (blue == 0) )
+        color = "green";
+    else if ( (red == 0) && (green == 0) && (blue == 1) )
+        color = "blue";
+    else if ( (red == 0) && (green == 0) && (blue == 0) )
+        color = "black";
+    else
+        color ="random";
+
+
+    if(addr == 14'b01100000000000)
+        $finish;
+
+
+    //$monitor("addr: %d r = %d g = %d b =%d\n", addr, red, green, blue);
+    $display("addr: %d color = %s\n", addr, color);
 end
 
 always #5 clk = ~clk;
